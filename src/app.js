2db504f2428984aefd66f225db692e0befe5be08
Jordan Majd <jordan@madtitanstudios.com>
2017-12-14 16:49:54 -0700

    Impl functionality to get org and repos and store in DB

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 'use strict';
 
+/* Imports */
+
+// configure environment variables from .env
+// ensure this runs before anything else
 require('dotenv').config();
 
-// initial function to set up CI and Mocha / Chai
-function app(){
-
-  return 'it works!';
-}
+const DB = require('./db');
+const GHClient = require('./ghclient');
 
 
-module.exports = app;
+/* Exports */
+// module.exports = app;
+
+let client = new GHClient();
+let db = new DB();
+
+client.get('orgs/' + process.env.GH_ORG)
+  .then(function(org){
+    db.store('orgs', org);
+    return client.getPaginated('orgs/ramda/repos', org.public_repos);
+  })
+  .then(function(repos){
+    repos.forEach(function(repo){
+      db.store('repo', repo);
+    });
+  });
