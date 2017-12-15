9af970bba2c4ba58286fc01355e3efc2643047ee
Jordan Majd <jordan@madtitanstudios.com>
2017-12-14 17:40:05 -0700

    Push so can have latest on other comp

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@
 // ensure this runs before anything else
 require('dotenv').config();
 
+const Promis = require('bluebird');
 const DB = require('./db');
 const GHClient = require('./ghclient');
 
@@ -15,14 +16,22 @@ const GHClient = require('./ghclient');
 
 let client = new GHClient();
 let db = new DB();
+let orgName = process.env.GH_ORG;
 
-client.get('orgs/' + process.env.GH_ORG)
-  .then(function(org){
-    db.store('orgs', org);
-    return client.getPaginated('orgs/ramda/repos', org.public_repos);
-  })
-  .then(function(repos){
-    repos.forEach(function(repo){
-      db.store('repo', repo);
-    });
+client.get('orgs/' + orgName)
+  .then(getReposForOrg)
+  .then(getPullsFromRepos);
+
+
+function getReposForOrg(org){
+  db.store('orgs', org);
+  return client.getPaginated('orgs/' + orgName + '/repos', org.public_repos);
+}
+
+function getPullsFromRepos(repos){
+  return Promise.map(repos, function(repo){
+    db.store('repo', repo);
+    let uri = repo.url + '/pulls';
+    return client.getPaginated(uri); //todo update so doesn't need end number
   });
+}
